54a916fcdfd3d34674036748329ee4fcb79b1651
Gaurav Meena <meenajigaurav9589@gmail.com>
2025-04-11 16:12:31 +0530

    new dom some jss

---
+++ practice1/script.js
@@ -0,0 +1,7 @@
+let heading = document.getElementById('head1')
+console.log(heading)
+let head = document.getElementsByClassName('head')[0].style.color="red"
+console.log(head)
+
+const newWay1  = document.querySelectorAll('.head').style.backgroundColor = "deepsky";
+let ne
