14fd3e1595b662f29afc17cb2f13bbd443ed3a36
Gaurav Meena <meenajigaurav9589@gmail.com>
2025-04-18 14:18:55 +0530

    colorrr

--- practice1/script.js
+++ practice1/script.js
@@ -1,7 +1,13 @@
-let heading = document.getElementById('head1')
-console.log(heading)
-let head = document.getElementsByClassName('head')[0].style.color="red"
-console.log(head)
+let heading = document.getElementById("head1");
+console.log(heading);
+let head = (document.getElementsByClassName("head")[0].style.color = "red");
+console.log(head);
+const newWay1 = document.querySelectorAll(".head")[0].style.color = "deepskyblue";
 
-const newWay1  = document.querySelectorAll('.head').style.backgroundColor = "deepsky";
-let ne
+const new1 = document.querySelectorAll(".bg")
+new1[0].style.backgroundColor = "green";
+new1[1].style.backgroundColor = "blue";
+new1[2].style.backgroundColor = "red";
+new1[3].style.backgroundColor = "orange";
+new1[4].style.backgroundColor = "pink";
+console.log(new1)
